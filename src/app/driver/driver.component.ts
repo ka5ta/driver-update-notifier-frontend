a432a9c1d89f4ca21afce6031a38f3f93f9f69b2
ka5ta <55314237+ka5ta@users.noreply.github.com>
2022-01-07 23:18:41 +0100

    correcting viuals of  drivers list

--- src/app/driver/driver.component.ts
+++ src/app/driver/driver.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
 import { DriverService } from '../service/driver.service';
 import { Driver } from '../model/driver';
 import { Product } from '../model/product';
@@ -16,14 +16,19 @@ export class DriverComponent implements OnInit {
   @Input() product?: Product;
   @Input() showSpinner: boolean = false;
 
+  downloadImageSrc: string = "assets/download-button.png";
+  imageAlt = "Link";
+
 
 
   constructor(private driverService: DriverService) { }
 
   ngOnInit(): void {
-
   }
+
 }
+
+
 
 
 
